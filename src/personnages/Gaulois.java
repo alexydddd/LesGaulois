bbde23105f07d6c0a307dedce818a40ec70e2962
alexydddd <alexydeconinck@gmail.com>
2026-03-15 18:27:20 +0100

    Implement potion effect in Gaulois class
    Added potion effect functionality to Gaulois class.

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,6 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int effetPotion = 1;
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -29,8 +30,15 @@ public class Gaulois {
 	}
 
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans "
-				+ "la mâchoire de " + romain.getNom());	
-		romain.recevoirCoup(force/3);
+	    System.out.println(nom + " envoie un grand coup dans la mâchoire de " 
+	    		+ romain.getNom());
+	    romain.recevoirCoup((force * effetPotion) / 3);  
+	    if (effetPotion > 1) {
+	        effetPotion--;  
+	    }
+	}
+	
+	public void boirePotion(int forcePotion) {
+		this.effetPotion = forcePotion;
 	}
 }
