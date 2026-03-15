1f5f55aae18c5b0feec7bd88ca439d3f455542f3
alexydddd <alexydeconinck@gmail.com>
2026-03-15 18:26:08 +0100

    Refactor Chaudron class and add new methods
    Refactored resterPotion method to return Boolean. Added remplirChaudron and prendreLouche methods for functionality.

--- src/personnages/Chaudron.java
+++ src/personnages/Chaudron.java
@@ -1,12 +1,23 @@
 package personnages;
 
 public class Chaudron {
-	private int quantitePotion;
-	private int forcePotion;
-	
-	public void resterPotion() {
-		if ( this.quantitePotion > 0 ) {
-			return true;
-		}
-	}
+    private int quantitePotion;
+    private int forcePotion;
+
+    public Boolean resterPotion() {
+        return quantitePotion != 0;
+    }
+
+    public Boolean remplirChaudron(int quantite, int forcePotion) {
+        this.quantitePotion = quantite;
+        this.forcePotion = forcePotion;
+        return true;
+    }
+
+    public int prendreLouche() {
+        if (quantitePotion > 0) {
+            quantitePotion--;
+        }
+        return forcePotion;
+    }
 }
