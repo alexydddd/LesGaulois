b7fa6c406ee442452ba2d97641adbd48c788f17f
alexydddd <alexydeconinck@gmail.com>
2026-03-15 18:28:01 +0100

    Refactor TestGaulois to include Druide and additional logic

--- src/test_fonctionne/TestGaulois.java
+++ src/test_fonctionne/TestGaulois.java
@@ -1,23 +1,35 @@
-package test_fonctionne;
+package test_fonctionnel;
 
 import personnages.Gaulois;
 import personnages.Romain;
+import personnages.Druide;
 
 public class TestGaulois {
-	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
-		Gaulois obelix = new Gaulois("Obélix", 16);
-		Romain minus = new Romain("Minus", 6);
-		
-		asterix.parler("Bonjour Obélix.");
-		obelix.parler("Bonjour Astérix. Ca te dirais "
-				+ "d'aller chasser des sangliers ?)");
-		asterix.parler("Oui très bonne idée.");
-		System.out.println("Dans la fôret" + asterix + "et" + 				obelix + "tombent nez à nez sur le romain" + 
-		minus + ".");
-		asterix.frapper(minus);
-		asterix.frapper(minus);
-		asterix.frapper(minus);
-	}
-	
+    public static void main(String[] args) {
+        
+        Gaulois asterix = new Gaulois("Astérix", 8);
+        Gaulois obelix = new Gaulois("Obélix", 16);
+        Romain minus = new Romain("Minus", 6);
+        Romain brutus = new Romain("Brutus", 14);
+        Druide panoramix = new Druide("Panoramix", 2);
+        
+        asterix.parler("Bonjour Obélix.");
+        obelix.parler("Bonjour Astérix. Ca te dirais d'aller chasser des sangliers ?)");
+        asterix.parler("Oui très bonne idée.");
+        
+        System.out.println("Dans la forêt " + asterix + " et " + obelix + 
+                           " tombent nez à nez sur le romain " + minus.getNom() + ".");
+        
+        for (int i = 0; i < 3; i++) {
+            asterix.frapper(minus);
+        }
+        
+        panoramix.fabriquerPotion(4, 3);
+        panoramix.boosterGaulois(obelix);
+        panoramix.boosterGaulois(asterix);
+        
+        for (int i = 0; i < 3; i++) {
+            asterix.frapper(brutus);
+        }
+    }
 }
