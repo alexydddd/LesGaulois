13844aea2e096cea28ff54a9cae671fbbabda4ab
alexydddd <alexydeconinck@gmail.com>
2026-03-15 18:26:49 +0100

    Enhance Druide class with potion functionality
    Added constructor, potion making, and boosting methods.

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,18 +1,49 @@
 package personnages;
 
 public class Druide {
-	private String nom;
-	private int force;
-	
-	public String getNom() {
-		return nom;
-	}
-	
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-	
-	private String prendreParole() {
-		return "Le Druide " + nom + " : ";
-	}
+    private String nom;
+    private int force;
+    private Chaudron chaudron;
+
+    public Druide(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+        this.chaudron = new Chaudron();  
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
+
+    private String prendreParole() {
+        return "Le Druide " + nom + " : ";
+    }
+
+    public void fabriquerPotion(int quantite, int forcePotion) {
+        chaudron.remplirChaudron(quantite, forcePotion);
+        parler("J'ai concocté " + quantite + 
+               " doses de potion magique. Elle a une force de " + 
+               forcePotion + ".");
+    }
+    
+    public void boosterGaulois(Gaulois gaulois) {
+        boolean contientPotion = chaudron.resterPotion();
+        String nomGaulois = gaulois.getNom();
+
+        if (contientPotion) {
+            if (nomGaulois.equals("Obélix")) {
+                parler("Non, " + nomGaulois + " Non !... Et tu le sais très bien !");
+            } else {
+                int forcePotion = chaudron.prendreLouche();
+                gaulois.boirePotion(forcePotion);
+                parler("Tiens " + nomGaulois + " un peu de potion magique.");
+            }
+        } else {
+            parler("Désolé " + nomGaulois + " il n'y a plus une seule goutte de potion.");
+        }
+    }
 }
