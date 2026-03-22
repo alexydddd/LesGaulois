7ac2d06228028748824311cdbb1d1d0f6f507d74
alexydddd <alexydeconinck@gmail.com>
2026-03-22 15:17:52 +0100

    Refactor Gaulois class with improved structure

--- src/village_gaulois/Gaulois.java
+++ src/village_gaulois/Gaulois.java
@@ -1,54 +1,77 @@
 package village_gaulois;
 
-import personnages.Romain;
-
 public class Gaulois {
-	private String nom;
-	private int force;
-	private int effetPotion = 1;
-	private Village village;
-	
-	public Gaulois(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
 
-	public String getNom() {
-		return nom;
-	}
-	
-	public void setVillage( Village village) {
-		this.village = village;
-	}
-	
-	@Override
-	public String toString() {
-		return "" + nom;
-	}
-	
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-	
-	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
-	
-	public void frapper(Romain romain) {
-	    System.out.println(nom + " envoie un grand coup dans la mâchoire de " 
-	    		+ romain.getNom());
-	    romain.recevoirCoup((force * effetPotion) / 3);  
-	    if (effetPotion > 1) {
-	        effetPotion--;  
-	    }
-	}
-	
-	public void boirePotion(int forcePotion) {
-		this.effetPotion = forcePotion;
-	}
-	
-	public String sePresenter() {
-		return "Je suis le Gaulois " + nom + " !";
-	}
-	
+    private String nom;
+    private Integer force;
+    private Village village;
+
+    // Constructeur
+    public Gaulois(String nom, Integer force) {
+        this.nom = nom;
+        this.force = force;
+        this.village = null;
+    }
+
+    // Getters / Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public Integer getForce() {
+        return force;
+    }
+
+    public Village getVillage() {
+        return village;
+    }
+
+    public void setVillage(Village village) {
+        this.village = village;
+    }
+
+    // Parler
+    public void parler(String texte) {
+        System.out.println(nom + " dit : " + texte);
+    }
+
+    // prendreParole (version sans argument)
+    private String prendreParole() {
+        return nom + " prend la parole.";
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return nom;
+    }
+
+    // frapper un romain
+    public void frapper(romain.Romain romain) {
+        System.out.println(nom + " frappe " + romain.getNom() + " !");
+        romain.recevoirCoup(force);
+    }
+
+    // boirePotion
+    public void boirePotion(Integer forcePotion) {
+        force += forcePotion;
+        parler("Par Toutatis !");
+    }
+
+    // sePresenter
+    public void sePresenter() {
+        if (village != null && village.getChef().equals(this)) {
+            // C'est le chef du village
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". Je suis le chef le village : " + village.getNom() + "\"");
+        } else if (village != null) {
+            // Habitant d'un village
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". J'habite le village : " + village.getNom() + "\"");
+        } else {
+            // Sans village
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". Je voyage de villages en villages.\"");
+        }
+    }
 }
